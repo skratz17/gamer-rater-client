b11f179830eb12d21e6a15b39e51b68fd7f0336c
Jacob Eckert <jweckert17@gmail.com>
2020-11-02 15:16:39 -0600

    loading and rendering basic game details in game detail view

--- src/games/GameDetail.js
+++ src/games/GameDetail.js
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+
+import { GameContext } from './GameProvider';
 
 export const GameDetail = () => {
   const { gameId } = useParams();
 
+  const { getGameById } = useContext(GameContext);
+
+  const [ game, setGame ] = useState(null);
+
+  useEffect(() => {
+    const _getGameById = async gameId => {
+      const _game = await getGameById(gameId);
+
+      setGame(_game);
+    }
+
+    _getGameById(gameId);
+  }, []);
+
+  if(game === null) {
+    return <div>loading...</div>;
+  }
+
   return (
-    <div>{gameId}</div>
+    <div className="game">
+      <h2 className="game__title">{game.title} ({game.year})</h2>
+      <p className="game__designer">{game.designer || 'Unknown Developer'}</p>
+      <p className="game__description">{game.description}</p>
+      <p className="game__num-players">Up to {game.num_players} players</p>
+      <p className="game__estimated-duration">Average game lasts {game.estimated_duration} minutes</p>
+      <p className="game__age-recommendation">Recommended for players {game.age_recommendation} years of age and up</p>
+    </div>
   )
 };
--- src/games/GameProvider.js
+++ src/games/GameProvider.js
@@ -13,8 +13,14 @@ export const GameProvider = props => {
     setGames(games);
   };
 
+  const getGameById = async gameId => {
+    const response = await request(`http://localhost:8000/games/${gameId}`);
+    const game = await response.json();
+    return game;
+  };
+
   return (
-    <GameContext.Provider value={{ games, getGames }}>
+    <GameContext.Provider value={{ games, getGames, getGameById }}>
       {props.children}
     </GameContext.Provider>
   );
