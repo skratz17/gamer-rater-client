18848e34f64537d4aec207daf4740ba2292191e1
Jacob Eckert <jweckert17@gmail.com>
2020-11-02 14:28:47 -0600

    implement GameList

---
+++ src/games/GameList.js
@@ -0,0 +1,20 @@
+import React, { useContext, useEffect } from 'react';
+import { GameContext } from './GameProvider';
+
+export const GameList = () => {
+  const { games, getGames } = useContext(GameContext);
+
+  useEffect(() => {
+    getGames();
+  }, []);
+
+  return (
+    <ul className="game-list">
+      {
+        games.map(game => (
+          <li key={game.id}>{game.name}</li>
+        ))
+      }
+    </ul>
+  );
+};
