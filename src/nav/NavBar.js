15e697c32e6d923f059175638cae327c22fa9276
Jacob Eckert <jweckert17@gmail.com>
2020-11-06 13:36:26 -0600

    add basic CSS reset, refactor a lot of NavBar

--- src/nav/NavBar.js
+++ src/nav/NavBar.js
@@ -1,37 +1,21 @@
-import React from "react"
-import { Link } from "react-router-dom"
-import "./NavBar.css"
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './NavBar.css';
 
-export const NavBar = (props) => {
-    return (
-        <ul className="navbar">
-            <li className="navbar__item">
-                Navigation link
-            </li>
-            <li className="navbar__item">
-                Navigation link
-            </li>
-            <li className="navbar__item">
-                Navigation link
-            </li>
-            {
-                (localStorage.getItem("gamer_rater_token") !== null) ?
-                    <li className="nav-item">
-                        <button className="nav-link fakeLink"
-                            onClick={() => {
-                                localStorage.removeItem("gamer_rater_token")
-                                props.history.push({ pathname: "/" })
-                            }}
-                        >Logout</button>
-                    </li> :
-                    <>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/login">Login</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/register">Register</Link>
-                        </li>
-                    </>
-            }        </ul>
-    )
-}
+export const NavBar = () => {
+  return (
+    <ul className="navbar">
+      <li className="navbar__item">
+          <Link to="/">Home</Link>
+      </li>
+      <li className="navbar__item">
+          <Link to="/games">Games</Link>
+      </li>
+      { localStorage.getItem('gamer_rater_token') !== null &&
+          <li className="navbar__item">
+            <Link to="/logout">Logout</Link>
+          </li> 
+      }
+    </ul>
+  )
+};
