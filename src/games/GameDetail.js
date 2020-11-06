f63d139ebefb367ac592cbc337cc7c2569d6e9dc
Jacob Eckert <jweckert17@gmail.com>
2020-11-06 15:11:26 -0600

    a lot of styling for detail page and images

--- src/games/GameDetail.js
+++ src/games/GameDetail.js
@@ -6,6 +6,7 @@ import { GameImageForm } from './GameImageForm';
 import { ReviewProvider } from '../reviews/ReviewProvider';
 import { ReviewList } from '../reviews/ReviewList';
 import { ReviewForm } from '../reviews/ReviewForm';
+import './GameDetail.css';
 
 export const GameDetail = () => {
   const { gameId } = useParams();
@@ -33,15 +34,25 @@ export const GameDetail = () => {
       <h2 className="game__title">{game.title} ({game.year})</h2>
       <p className="game__designer">{game.designer ? game.designer.name : 'Unknown Developer'}</p>
       <p className="game__description">{game.description}</p>
-      <p className="game__num-players">Up to {game.num_players} players</p>
-      <p className="game__estimated-duration">Average game lasts {game.estimated_duration} minutes</p>
-      <p className="game__age-recommendation">Recommended for players {game.age_recommendation} years of age and up</p>
-      {
-        game.categories.map(c => <p key={c.id} className="game__category">{c.name}</p>)
-      }
-      <p className="game__average-rating">Average rating: {game.average_rating} / 10</p>
+      <ul className="game__quick-facts">
+        <li className="game__num-players">Up to {game.num_players} players.</li>
+        <li className="game__estimated-duration">Average game lasts {game.estimated_duration} minutes.</li>
+        <li className="game__age-recommendation">Recommended for players {game.age_recommendation} years of age and up.</li>
+      </ul>
 
-      {game.images.map(({ image }) => <img src={image} />)}
+      <div className="game__additional-info">
+        <ul className="game__categories">
+          <li>Categories:</li>
+          {
+            game.categories.map(c => <li key={c.id} className="game__category">{c.name}</li>)
+          }
+        </ul>
+        <p className="game__average-rating">Average rating: {game.average_rating} / 10</p>
+      </div>
+
+      <div className="game__images">
+        {game.images.map(({ image }) => <img className="game__image" src={image} />)}
+      </div>
 
       <GameImageForm gameId={game.id} onUploadSuccess={() => _getGameById(gameId)} />
 
--- src/games/GameImageForm.js
+++ src/games/GameImageForm.js
@@ -1,5 +1,7 @@
 import React, { useContext } from 'react';
+
 import { GameContext } from './GameProvider';
+import './GameImageForm.css';
 
 export const GameImageForm = props => {
   const { gameId, onUploadSuccess } = props;
@@ -22,9 +24,12 @@ export const GameImageForm = props => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input type="file" id="game_image" />
-      <button type="submit">Upload</button>
-    </form>
+    <>
+      <h3 className="game-image-form__header">Upload your own photo!</h3>
+      <form className="game-image-form" onSubmit={handleSubmit}>
+        <input className="game-image-form__file" type="file" id="game_image" />
+        <button className="game-image-form__submit btn btn--create" type="submit">Upload</button>
+      </form>
+    </>
   );
 };
