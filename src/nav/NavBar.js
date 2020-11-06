a943e6d655e99bae26c6f4fdc1299d38c64a48b6
Jacob Eckert <jweckert17@gmail.com>
2020-11-06 14:04:02 -0600

    basic navbar styling

--- src/nav/NavBar.js
+++ src/nav/NavBar.js
@@ -1,21 +1,23 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import './NavBar.css';
 
 export const NavBar = () => {
   return (
-    <ul className="navbar">
-      <li className="navbar__item">
-          <Link to="/">Home</Link>
-      </li>
-      <li className="navbar__item">
-          <Link to="/games">Games</Link>
-      </li>
-      { localStorage.getItem('gamer_rater_token') !== null &&
-          <li className="navbar__item">
-            <Link to="/logout">Logout</Link>
-          </li> 
-      }
-    </ul>
+    <nav className="navbar">
+      <ul className="navbar-list">
+        <li className="navbar__item">
+            <Link to="/">Home</Link>
+        </li>
+        <li className="navbar__item">
+            <Link to="/games">Games</Link>
+        </li>
+        { localStorage.getItem('gamer_rater_token') !== null &&
+            <li className="navbar__item">
+              <Link to="/logout">Logout</Link>
+            </li> 
+        }
+      </ul>
+    </nav>
   )
 };
